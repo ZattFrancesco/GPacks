0f6fb2872344b04f9f1da31f34522467a696c1b7
ZattFrancesco <francesco.zattolo@student.hepl.be>
2026-03-13 12:05:49 +0100

    /ping update

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,12 +1,22 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Affiche la latence du bot"),
+    .setName('ping')
+    .setDescription('Affiche la latence du bot')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    
   async execute(interaction) {
-    const sent = await interaction.reply({ content: "Pong...", fetchReply: true });
-    const latency = sent.createdTimestamp - interaction.createdTimestamp;
-    await interaction.editReply(`🏓 Pong ! Latence: ${latency}ms`);
-  }
+    const sent = await interaction.reply({
+      content: '🏓 Calcul de la latence...',
+      fetchReply: true,
+    });
+
+    const messageLatency = sent.createdTimestamp - interaction.createdTimestamp;
+    const apiLatency = interaction.client.ws.ping;
+
+    await interaction.editReply(
+      `**🏓 Pong !**\nLatence message :** ${messageLatency}ms**\nLatence API :** ${apiLatency}ms**`
+    );
+  },
 };
