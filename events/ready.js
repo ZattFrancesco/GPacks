16f708f2f492a706388994d528d82371fb324065
Zattolo Francesco <francesco.zattolo@student.hepl.be>
2026-03-24 10:05:28 +0100

    Bug fix

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,4 @@
 const logger = require("../src/utils/logger");
-const { sendBotLogToAllGuilds, DEFAULT_COLORS, lines } = require("../src/utils/discordLogs");
 
 module.exports = {
   name: "clientReady",
@@ -7,16 +6,5 @@ module.exports = {
   async execute(client) {
     logger.info(`Connecté en tant que ${client.user.tag}`);
     logger.info(`Serveurs: ${client.guilds.cache.size}`);
-
-    await sendBotLogToAllGuilds(client, {
-      color: DEFAULT_COLORS.success,
-      title: '🟢 Bot démarré',
-      description: lines([
-        `**Bot** : ${client.user.tag}`,
-        `**Serveurs** : **${client.guilds.cache.size}**`,
-        `**Heure** : <t:${Math.floor(Date.now() / 1000)}:F>`,
-      ]),
-      footer: 'Ghost\'Packs • Bot logs',
-    }).catch(() => {});
   }
 };
